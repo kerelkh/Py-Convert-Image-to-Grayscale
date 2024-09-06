6ba5cec087f018cc4a0b35eb02a87edb0f36fdca
kerelkh <kerelkh@gmail.com>
2024-09-06 23:52:28 +0700

    layouting image to make it more modern look

--- Converter.py
+++ Converter.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 import io
 
-st.title("Image to Grayscale")
-st.subheader('Convert Image to Grayscale')
+st.title("Grayscale Converter")
+st.write('Select image file to convert into grayscale image')
 
 with st.expander('Upload Image'):
     file = st.file_uploader('Upload Image', type=['png', 'jpg', 'jpeg'])
@@ -11,15 +11,22 @@ with st.expander('Upload Image'):
 if file:
     image = Image.open(file)
     grayscale = image.convert('L')
-    st.image(grayscale)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Result:")
+        st.image(grayscale)
 
-    img_byte_arr = io.BytesIO()
-    grayscale.save(img_byte_arr, format=image.format)
-    img_byte_arr = img_byte_arr.getvalue()
+        img_byte_arr = io.BytesIO()
+        grayscale.save(img_byte_arr, format=image.format)
+        img_byte_arr = img_byte_arr.getvalue()
 
-    st.download_button(
-        label='Download Image',
-        data=img_byte_arr,
-        file_name=f"download_image.{image.format.lower()}",
-        mime=f"image/{image.format.lower()}"
-    )
+        st.download_button(
+            label='Download Image',
+            data=img_byte_arr,
+            file_name=f"download_image.{image.format.lower()}",
+            mime=f"image/{image.format.lower()}"
+        )
+
+    with col2:
+        st.write("original:")
+        st.image(image)
